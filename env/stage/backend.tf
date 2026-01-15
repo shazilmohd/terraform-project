0ee4f7f1f214068a6de106e53211c722a8eeb0c8
shazil <shaazilmohd@gmail.com>
2026-01-16 00:56:34 +0530

    Add: S3 remote backend configuration for Terraform state persistence

---
+++ env/stage/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-1768505102"
+    key            = "stage/terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "terraform-locks"
+  }
+}
